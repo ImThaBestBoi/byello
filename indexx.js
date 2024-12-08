589bc5d97e6087115117b3015dac9ffd5b3643cd
Nameless <80163094+ImThaBestBoi@users.noreply.github.com>
2024-12-08 22:42:51 +0530

    Add files via upload

---
+++ indexx.js
@@ -0,0 +1,8 @@
+// console.log(`yooo`)
+// console.log(`heloo sorry`)
+
+// window.alert(`this is warning`)
+// window.alert(`You are being hacked`)
+document.getElementById('h1').textContent = "HELLO";
+document.getElementById('p').textContent = "Press this button";
+document.getElementById('b').textContent = "BYE";
---
+++ jaa.js
@@ -0,0 +1,3 @@
+document.getElementById('h1').textContent = "BYE";
+document.getElementById('p').textContent = "Press this button";
+document.getElementById('b').textContent = "HELLO";
